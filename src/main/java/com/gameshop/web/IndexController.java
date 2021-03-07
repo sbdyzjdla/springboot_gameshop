9caa5671a39c332dbd61bd49dedd9e11f7038e1c
sbdyzjdla <sbdyzjdla@gmail.com>
2021-03-08 02:03:30 +0900

    Oauth2 구글 로그인

--- src/main/java/com/gameshop/web/IndexController.java
+++ src/main/java/com/gameshop/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.gameshop.web;
 
+import com.gameshop.config.auth.dto.SessionUser;
 import com.gameshop.service.QnasService;
 import com.gameshop.web.dto.QnasResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,14 +9,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final QnasService qnasService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
+
+        SessionUser user = (SessionUser) httpSession
+                .getAttribute("user");
+
+        if(user != null) {
+            model.addAttribute("userName" , user.getName());
+        }
         return "index";
     }
 
