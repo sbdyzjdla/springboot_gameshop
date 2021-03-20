d08681f36b532d4dd3cb09b1c81f95d079b91487
sbdyzjdla <sbdyzjdla@gmail.com>
2021-03-20 23:43:08 +0900

    file 저장경로 추가

--- src/main/java/com/gameshop/service/FilesService.java
+++ src/main/java/com/gameshop/service/FilesService.java
@@ -3,6 +3,8 @@ package com.gameshop.service;
 import com.gameshop.domain.files.FilesRepository;
 import com.gameshop.web.dto.FilesSaveRequestDto;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -10,11 +12,15 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.util.UUID;
 
+@PropertySource("classpath:application.properties")
 @RequiredArgsConstructor
 @Service
 public class FilesService {
 
     private final FilesRepository filesRepository;
+    @Value("${image.route.win}")
+    private String baseDir;
+
 
     @Transactional
     public Long save(MultipartFile file, String owner) {
@@ -24,8 +30,8 @@ public class FilesService {
         String origin_filename = file.getOriginalFilename();
         String save_filename = savename.toString();
 
-        String baseDir = "C:\\쇼핑몰이미지";
-        String filePath = baseDir + "\\" + origin_filename;
+        //String baseDir = "C:\\쇼핑몰이미지";
+        String filePath = baseDir + "\\" + save_filename;
 
         try {
             file.transferTo(new File(filePath));
