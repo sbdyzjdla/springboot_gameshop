c945678ec656d57526d8db84f9d11d0f2bd0cd96
yoonsung <sbdyzjdla@gmail.com>
2021-05-03 23:45:35 +0900

    QnasApi Save Test   (스프링시큐리티 적용후)

--- src/test/java/com/gameshop/web/QnasApiControllerTest.java
+++ src/test/java/com/gameshop/web/QnasApiControllerTest.java
@@ -1,26 +1,32 @@
 package com.gameshop.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gameshop.domain.qnas.Qnas;
 import com.gameshop.domain.qnas.QnasRepository;
 import com.gameshop.web.dto.QnasSaveRequestDto;
 import com.gameshop.web.dto.QnasUpdateRequestDto;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-import javax.xml.ws.Response;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -33,6 +39,19 @@ public class QnasApiControllerTest {
     private TestRestTemplate restTemplate;
 
     @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @Before
+    public void setup(){
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Autowired
     private QnasRepository qnasRepository;
 
     @After
@@ -41,6 +60,7 @@ public class QnasApiControllerTest {
     }
 
     @Test
+    @WithMockUser(roles = "ADMIN")
     public void qna_게시글저장() throws Exception{
         //given
         String title = "테스트 게시글";
@@ -56,13 +76,23 @@ public class QnasApiControllerTest {
 
         String url = "http://localhost:" + port + "/api/v1/qnas";
 
+        /*       TestRestTemplate 쓰는경우
         //when
         ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
 
         //then
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(responseEntity.getBody()).isGreaterThan(0L);
+        */
 
+        //when        springSecurity 적용할시 테스트 코드
+        mvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(requestDto)))
+                .andExpect(status().isOk());
+
+
+        //then
         List<Qnas> all = qnasRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(title);
         assertThat(all.get(0).getAuthor()).isEqualTo(author);
