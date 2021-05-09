a8b0854ad1b2ac44ad1b80689c316d7fcfe7b4d3
yoonsung <sbdyzjdla@gmail.com>
2021-05-09 20:46:38 +0900

    로컬, 서버 파일저장 경로 분리 설정 test application.properties

--- src/test/java/com/gameshop/GameshopApplicationTests.java
+++ src/test/java/com/gameshop/GameshopApplicationTests.java
@@ -1,6 +1,7 @@
 package com.gameshop;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
