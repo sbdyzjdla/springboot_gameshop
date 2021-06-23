f489920f722051b92ac4afa6d406f73748591ffb
yoonsung <sbdyzjdla@gmail.com>
2021-06-24 00:13:03 +0900

    counter

--- out/production/resources/static/js/custom.js
+++ out/production/resources/static/js/custom.js
@@ -246,50 +246,10 @@
 //  }
 //})();
 
-//inputNumber($('.input-number'));
-
-// (function() {
-// window.inputNumber = function(el) {
-//    var min = el.attr('min') || false;
-//    var max = el.attr('max') || false;
-//
-//    var els = {};
-//
-//    els.dec = el.prev();
-//    els.inc = el.next();
-//
-//    el.each(function() {
-//      init($(this));
-//    });
-//
-//    function init(el) {
-//
-//      els.dec.on('click', decrement);
-//      els.inc.on('click', increment);
-//
-//      function decrement() {
-//        var value = el[0].value;
-//        value--;
-//        (value<1) ? value=1 : '';
-//        if(!min || value >= min) {
-//          el[0].value = value;
-//        }
-//      }
-//
-//      function increment() {
-//        var value = el[0].value;
-//        value++;
-//        if(!max || value <= max) {
-//          el[0].value = value++;
-//        }
-//      }
-//    }
-//  };
-//})();
-//
 //$('.input-number').each(function(){
 //	inputNumber($(this));
 //})
+
 
   setInterval(function () {
     makeTimer();
@@ -316,5 +276,3 @@
  });
 
 }(jQuery));
-
-
--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -206,48 +206,49 @@
 
   }
 // click counter js
-(function() {
- 
-  window.inputNumber = function(el) {
+//(function() {
+//
+//  window.inputNumber = function(el) {
+//
+//    var min = el.attr('min') || false;
+//    var max = el.attr('max') || false;
+//
+//    var els = {};
+//
+//    els.dec = el.prev();
+//    els.inc = el.next();
+//
+//    el.each(function() {
+//      init($(this));
+//    });
+//
+//    function init(el) {
+//
+//      els.dec.on('click', decrement);
+//      els.inc.on('click', increment);
+//
+//      function decrement() {
+//        var value = el[0].value;
+//        value--;
+//        if(!min || value >= min) {
+//          el[0].value = value;
+//        }
+//      }
+//
+//      function increment() {
+//        var value = el[0].value;
+//        value++;
+//        if(!max || value <= max) {
+//          el[0].value = value++;
+//        }
+//      }
+//    }
+//  }
+//})();
 
-    var min = el.attr('min') || false;
-    var max = el.attr('max') || false;
-
-    var els = {};
-
-    els.dec = el.prev();
-    els.inc = el.next();
-
-    el.each(function() {
-      init($(this));
-    });
-
-    function init(el) {
-
-      els.dec.on('click', decrement);
-      els.inc.on('click', increment);
-
-      function decrement() {
-        var value = el[0].value;
-        value--;
-        if(!min || value >= min) {
-          el[0].value = value;
-        }
-      }
-
-      function increment() {
-        var value = el[0].value;
-        value++;
-        if(!max || value <= max) {
-          el[0].value = value++;
-        }
-      }
-    }
-  }
-})();
-
-inputNumber($('.input-number'));
-
+//$('.input-number').each(function(){
+//	inputNumber($(this));
+//})
 
 
   setInterval(function () {
