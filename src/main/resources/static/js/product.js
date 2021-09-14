dfdf05c04a4254d525b215139fcb44993100bbcc
yoonsung <sbdyzjdla@gmail.com>
2021-09-14 11:38:41 +0900

    상품더보기(모든상품적용)

--- src/main/resources/static/js/product.js
+++ src/main/resources/static/js/product.js
@@ -6,18 +6,23 @@ var product = {
             _this.list_nin_console();
         })
 
-        $('#more_products').on('click', function(e) {
+        $('#more_consoles').on('click', function(e) {
             e.preventDefault();
-            _this.list_nin_console_more();
+            _this.list_consoles_more();
+        })
+
+        $('#more_titles').on('click', function(e) {
+            e.preventDefault();
+            _this.list_titles_more();
         })
 
     },
 
-    list_nin_console : function() {
-
+    list_console : function() {
+        var url = $('#products_url').val();
         $.ajax({
             type : 'GET',
-            url : '/api/v1/consoles/nintendoCList/',
+            url : '/api/v1/consoles/' + url ,
             dataType : 'json',
         }).done(function(success) {
         $('#product_list').children().remove();
@@ -31,9 +36,9 @@ var product = {
                      +       '</div>'
                      +  '</div>');
             }
-            $('#more_products').children().remove();
+            $('#more_consoles').children().remove();
             if(success.last == false) {
-                $('#more_products').append(
+                $('#more_consoles').append(
                     '<a href=\"#\" class=\"btn_3 col-md-2\">더보기</a>'
                     + '<input type=\"hidden\" id=\"products_page\" value=\"1\">'
                 );
@@ -44,105 +49,98 @@ var product = {
         });
     },
 
-    list_nin_console_more : function() {
+    list_consoles_more : function() {
         var p_num = $('#products_page').val();
+        var url = $('#products_url').val();
         $.ajax({
-                    type : 'GET',
-                    url : '/api/v1/consoles/nintendoCList/' + p_num,
-                    dataType : 'json',
-                }).done(function(success) {
-                var product_list = success;
-                    for(var i in product_list.content) {
-                         $('#product_list').append('<div class=\"col-lg-4 col-sm-4\">'
-                             + '<div class=\"single_product_item text-center\" onclick=\"product.view(' + product_list.content[i].id + ');\">'
-                             +       '<img src=\"/display/'+product_list.content[i].img_num+'\" alt=\"#\" class=\"img-fluid-list\">'
-                             +       '<h3> ' + product_list.content[i].p_name + ' </h3>'
-                             +       '<p>' + product_list.content[i].p_price + '원</p>'
-                             +       '</div>'
-                             +  '</div>');
-                    }
-                    $('#more_products').children().remove();
-                    if(success.last == false) {
-                        $('#more_products').append(
-                            '<a href=\"#\" class=\"btn_3 col-md-2\">더보기</a>'
-                            + '<input type=\"hidden\" id=\"products_page\" value=\"'+ success.number + 1 + '\">'
-                        );
-                        document.querySelector('#products_page').value = success.number+1;
-                    }
-                }).fail(function(error) {
-                    alert('게시글을 불러올수 없습니다');
-                });
+            type : 'GET',
+            url : '/api/v1/consoles/'+ url + p_num,
+            dataType : 'json',
+        }).done(function(success) {
+        var product_list = success;
+            for(var i in product_list.content) {
+                 $('#product_list').append('<div class=\"col-lg-4 col-sm-4\">'
+                     + '<div class=\"single_product_item text-center\" onclick=\"product.view(' + product_list.content[i].id + ');\">'
+                     +       '<img src=\"/display/'+product_list.content[i].img_num+'\" alt=\"#\" class=\"img-fluid-list\">'
+                     +       '<h3> ' + product_list.content[i].p_name + ' </h3>'
+                     +       '<p>' + product_list.content[i].p_price + '원</p>'
+                     +       '</div>'
+                     +  '</div>');
+            }
+            $('#more_consoles').children().remove();
+            if(success.last == false) {
+                $('#more_consoles').append(
+                    '<a href=\"#\" class=\"btn_3 col-md-2\">더보기</a>'
+                    + '<input type=\"hidden\" id=\"products_page\" value=\"'+ success.number + 1 + '\">'
+                );
+                document.querySelector('#products_page').value = success.number+1;
+            }
+        }).fail(function(error) {
+            alert('게시글을 불러올수 없습니다');
+        });
     },
 
-    list_ps5_console : function() {
+    list_titles : function() {
+        var url = $('#products_url').val();
+        $.ajax({
+            type : 'GET',
+            url : '/api/v1/titles/' + url ,
+            dataType : 'json',
+        }).done(function(success) {
+        $('#product_list').children().remove();
+        var product_list = success;
+            for(var i in product_list.content) {
+                 $('#product_list').append('<div class=\"col-lg-4 col-sm-4\">'
+                     + '<div class=\"single_product_item text-center\" onclick=\"product.view(' + product_list.content[i].id + ');\">'
+                     +       '<img src=\"/display/'+product_list.content[i].img_num+'\" alt=\"#\" class=\"img-fluid-list\">'
+                     +       '<h3> ' + product_list.content[i].p_name + ' </h3>'
+                     +       '<p>' + product_list.content[i].p_price + '원</p>'
+                     +       '</div>'
+                     +  '</div>');
+            }
+            $('#more_titles').children().remove();
+            if(success.last == false) {
+                $('#more_titles').append(
+                    '<a href=\"#\" class=\"btn_3 col-md-2\">더보기</a>'
+                    + '<input type=\"hidden\" id=\"products_page\" value=\"1\">'
+                );
+                document.querySelector('#products_page').value = success.number+1;
+            }
+        }).fail(function(error) {
+            alert('게시글을 불러올수 없습니다');
+        });
+    },
 
-            $.ajax({
-                type : 'GET',
-                url : '/api/v1/consoles/ps5CList',
-                dataType : 'json',
-            }).done(function(success) {
-            $('#product_list').children().remove();
-            var product_list = success;
-                for(var i in product_list) {
-                     $('#product_list').append('<div class=\"col-lg-4 col-sm-4\">'
-                         + '<div class=\"single_product_item text-center\" onclick=\"product.view(' + product_list[i].id + ');\">'
-                         +       '<img src=\"/display/'+product_list[i].img_num+'\" alt=\"#\" class=\"img-fluid-list\">'
-                         +       '<h3> ' + product_list[i].p_name + ' </h3>'
-                         +       '<p>' + product_list[i].p_price + '원</p>'
-                         +       '</div>'
-                         +  '</div>');
-                }
-            }).fail(function(error) {
-                alert('게시글을 불러올수 없습니다');
-            });
-        },
-
-    list_ns_software : function() {
-
-            $.ajax({
-                type : 'GET',
-                url : '/api/v1/titles/nsSoftList',
-                dataType : 'json',
-            }).done(function(success) {
-            $('#product_list').children().remove();
-            var product_list = success;
-                for(var i in product_list) {
-                     $('#product_list').append('<div class=\"col-lg-4 col-sm-4\">'
-                         + '<div class=\"single_product_item text-center\" onclick=\"product.view(' + product_list[i].id + ');\">'
-                         +       '<img src=\"/display/'+product_list[i].img_num+'\" alt=\"#\" class=\"img-fluid-list\">'
-                         +       '<h3> ' + product_list[i].p_name + ' </h3>'
-                         +       '<p>' + product_list[i].p_price + '원</p>'
-                         +       '</div>'
-                         +  '</div>');
-                }
-            }).fail(function(error) {
-                alert('게시글을 불러올수 없습니다');
-            });
-        },
-
-    list_ps5_software : function() {
-
-                $.ajax({
-                    type : 'GET',
-                    url : '/api/v1/titles/ps5SoftList',
-                    dataType : 'json',
-                }).done(function(success) {
-                $('#product_list').children().remove();
-                var product_list = success;
-                    for(var i in product_list) {
-                         $('#product_list').append('<div class=\"col-lg-4 col-sm-4\">'
-                             + '<div class=\"single_product_item text-center\" onclick=\"product.view(' + product_list[i].id + ');\">'
-                             +       '<img src=\"/display/'+product_list[i].img_num+'\" alt=\"#\" class=\"img-fluid-list\">'
-                             +       '<h3> ' + product_list[i].p_name + ' </h3>'
-                             +       '<p>' + product_list[i].p_price + '원</p>'
-                             +       '</div>'
-                             +  '</div>');
-                    }
-                }).fail(function(error) {
-                    alert('게시글을 불러올수 없습니다');
-                });
-            },
-
+    list_titles_more : function() {
+        var p_num = $('#products_page').val();
+        var url = $('#products_url').val();
+        $.ajax({
+            type : 'GET',
+            url : '/api/v1/titles/'+ url + p_num,
+            dataType : 'json',
+        }).done(function(success) {
+        var product_list = success;
+            for(var i in product_list.content) {
+                 $('#product_list').append('<div class=\"col-lg-4 col-sm-4\">'
+                     + '<div class=\"single_product_item text-center\" onclick=\"product.view(' + product_list.content[i].id + ');\">'
+                     +       '<img src=\"/display/'+product_list.content[i].img_num+'\" alt=\"#\" class=\"img-fluid-list\">'
+                     +       '<h3> ' + product_list.content[i].p_name + ' </h3>'
+                     +       '<p>' + product_list.content[i].p_price + '원</p>'
+                     +       '</div>'
+                     +  '</div>');
+            }
+            $('#more_titles').children().remove();
+            if(success.last == false) {
+                $('#more_titles').append(
+                    '<a href=\"#\" class=\"btn_3 col-md-2\">더보기</a>'
+                    + '<input type=\"hidden\" id=\"products_page\" value=\"'+ success.number + 1 + '\">'
+                );
+                document.querySelector('#products_page').value = success.number+1;
+            }
+        }).fail(function(error) {
+            alert('게시글을 불러올수 없습니다');
+        });
+    },
 
     view : function(id) {
             location.href = '/products/view/' + id;
